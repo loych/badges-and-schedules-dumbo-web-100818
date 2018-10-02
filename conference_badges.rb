ae0da897045e4e5eddb5778002155d4292de6aa0
IS Choi <insil321@hotmail.com>
2018-10-02 01:38:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creater(attendees)
+  arrayattendees= []
+  attendees.each do |attendes|
+  arrayattendees.push ("Hello, my name is #{attendees}.")
+end
+  return arrayattendees
+end
+
+def assign_rooms (attendees, room_assignment)
+  return "Hello, #{attendees}! You'll be assigned to room #{room_assignment}!"
+  hash= Hash.new
+  assign_rooms.each_with_index {|attendees, room_assignment|
+  hash[attendees]=room_assignment
+  }
+  hash
+end
+
+def printer 
+end
