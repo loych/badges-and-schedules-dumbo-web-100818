e24fb03a24a68a9477d07ee9d3cd1887fa59b0a5
IS Choi <insil321@hotmail.com>
2018-10-02 01:39:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creater(attendees)
+def batch_badge_creator(attendees)
   arrayattendees= []
   attendees.each do |attendees|
   arrayattendees.push ("Hello, my name is #{attendees}.")
