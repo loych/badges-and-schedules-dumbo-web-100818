094aebd46b41b4941cc91108ac959f08c9cdd0db
IS Choi <insil321@hotmail.com>
2018-10-02 01:59:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,10 +22,10 @@ return arrayattendees
 end
 
 def printer (attendees)
-  puts batch_badge_creator (attendees).each do |line|
+  batch_badge_creator (attendees).each do |line|
     
   end
- puts  assign_rooms (attendees).each do |line|
+  assign_rooms (attendees).each do |line|
 
   end
 end
